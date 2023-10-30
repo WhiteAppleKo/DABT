1fd164c3abb73f33a65770c5766ad12625529f93
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2023-10-30 13:28:13 +0900

    23.10.30
    github push test

---
+++ Assets/01.Scripts/Equipment/CreateEquipment.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEquipment : MonoBehaviour
+{
+    public GameObject obj;
+    public void CreateWeapon(){
+        
+    }
+}
